bb684b8d81a54c8c414a006dd1addfed70bf88ee
superdan427 <xudaniel3@gmail.com>
2025-12-13 03:40:43 +0000

    Fix coordinates for date spots
    Updated latitude and longitude coordinates for various locations in dateSpots.js.

--- data/dateSpots.js
+++ data/dateSpots.js
@@ -6,8 +6,8 @@ export const dateSpots = [
     category: "drinks",
     vibe: "Fusion cocktails",
     description: "Went there after Hiten and Rush’s exhibition.",
-    lat: 51.549,
-    lng: 0.0754,
+    lat: 51.53008,
+    lng: -0.07732,
     images: []
   },
   {
@@ -15,7 +15,7 @@ export const dateSpots = [
     category: "drinks",
     vibe: "Cool, dark interiors",
     description: "Indian fusion cocktails.",
-    lat: 51.5133,
+    lat: 51.51089,
     lng: -0.1336,
     images: []
   },
@@ -24,9 +24,9 @@ export const dateSpots = [
     category: "drinks",
     vibe: "London creatives’ final boss",
     description:
-      'Starter pack for any dickhead that introduces themselves as a “creative”, the most unbearable cunt.',
-    lat: 51.55,
-    lng: 0.08,
+      'starter pack  for any dickhead that introduces themselves as a creative"". the  most unbearable cunt you will evr meet. (I might have been there quite a few times...)',
+    lat: 51.54524,
+    lng: -0.07637,
     images: []
   },
   {
@@ -34,8 +34,8 @@ export const dateSpots = [
     category: "drinks",
     vibe: "Good, cosy atmosphere",
     description: "Went with Amber.",
-    lat: 51.53,
-    lng: -0.06,
+    lat: 51.52949,
+    lng: -0.05618,
     images: []
   },
   {
@@ -43,8 +43,8 @@ export const dateSpots = [
     category: "drinks",
     vibe: "Vinyl music, cosy atmosphere",
     description: "Doesn’t have space for 8 on a walk-in.",
-    lat: 51.519,
-    lng: -0.083,
+    lat: 51.5354,
+    lng: -0.1261,
     images: []
   },
   {
@@ -52,8 +52,8 @@ export const dateSpots = [
     category: "food",
     vibe: "Intimate setting",
     description: "Suggested by my amazing cousin.",
-    lat: 51.546,
-    lng: -0.075,
+    lat: 51.54306,
+    lng: -0.07581,
     images: []
   },
   {
@@ -61,26 +61,18 @@ export const dateSpots = [
     category: "drinks",
     vibe: "Cool interior",
     description: "I like my Scarfes.",
-    lat: 51.5176,
-    lng: -0.1219,
+    lat: 51.51733,
+    lng: -0.11776,
     images: []
   },
-  {
-    name: "Dove",
-    category: "food",
-    vibe: "Good food",
-    description: 'Previously “Orsay”. Good food.',
-    lat: 51.489,
-    lng: -0.222,
-    images: []
-  },
+
   {
     name: "Gold Notting Hill",
     category: "food",
     vibe: "Good food and cocktails",
     description: "Gotta have the sheep’s ricotta sourdough.",
-    lat: 51.516,
-    lng: -0.204,
+    lat: 51.51292,
+    lng: -0.20269,
     images: []
   },
   {
@@ -88,8 +80,8 @@ export const dateSpots = [
     category: "food",
     vibe: "Cheap, good food and setting",
     description: "Cheap Michelin Guide.",
-    lat: 51.505409,
-    lng: -0.090128,
+    lat: 51.50523,
+    lng: -0.08962,
     images: []
   },
   {
@@ -97,8 +89,8 @@ export const dateSpots = [
     category: "food",
     vibe: "Open kitchen, great pasta",
     description: "Comforting pasta.",
-    lat: 51.524011,
-    lng: -0.080561,
+    lat: 51.52678,
+    lng: -0.08038,
     images: []
   },
   {
@@ -106,8 +98,8 @@ export const dateSpots = [
     category: "food",
     vibe: "Nice atmosphere",
     description: "Good flatbread and fontina.",
-    lat: 51.514,
-    lng: -0.13,
+    lat: 51.5215,
+    lng: -0.20872,
     images: []
   },
   {
@@ -115,8 +107,8 @@ export const dateSpots = [
     category: "food",
     vibe: "Thai fusion food",
     description: "Went for a sextuple date, good pork belly.",
-    lat: 51.5224,
-    lng: -0.0747,
+    lat: 51.52422,
+    lng: -0.07686,
     images: []
   },
   {
@@ -124,8 +116,8 @@ export const dateSpots = [
     category: "food",
     vibe: "Spanish food, good setting",
     description: "Nice octopus.",
-    lat: 51.5114,
-    lng: -0.1397,
+    lat: 51.51141,
+    lng: -0.13967,
     images: []
   },
   {
@@ -133,8 +125,8 @@ export const dateSpots = [
     category: "food",
     vibe: "Very nice interior, good cocktails too",
     description: "The chai is not free refills.",
-    lat: 51.525535,
-    lng: -0.081342,
+    lat: 51.52516,
+    lng: -0.08087,
     images: []
   },
   {
@@ -142,8 +134,8 @@ export const dateSpots = [
     category: "food",
     vibe: "Portuguese food, very filling",
     description: "Good homemade sauces, ordered way too much food.",
-    lat: 51.504892,
-    lng: -0.092748,
+    lat: 51.51132,
+    lng: -0.13991,
     images: []
   },
   {
@@ -151,8 +143,8 @@ export const dateSpots = [
     category: "drinks",
     vibe: "Lovely interior",
     description: "Red interior — need I say more.",
-    lat: 51.52,
-    lng: -0.13,
+    lat: 51.51717,
+    lng: -0.12903,
     images: []
   },
   {
@@ -170,8 +162,8 @@ export const dateSpots = [
     vibe: "Let me land. Good walk.",
     description:
       "I know — a cemetery — but it’s beautiful. Can walk next to the Thames after.",
-    lat: 51.4876,
-    lng: -0.1876,
+    lat: 51.48294,
+    lng: -0.18604,
     images: []
   },
   {
